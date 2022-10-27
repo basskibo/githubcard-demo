9a09c74b4884f9829fdf9f4ddbb2dd3ce560d2e9
Bojan <jagetic.bojan@gmail.com>
2022-10-27 16:34:06 +0200

    feat: added react-helmet

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Helmet } from "react-helmet"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Helmet>
+    <script async defer data-website-id="32b4f7e4-f30c-47e1-864a-ee4f1a4a0cbb" src="https://blogtech-umami.herokuapp.com/umami.js"></script>
+    </Helmet>
     <App />
   </React.StrictMode>
 );
